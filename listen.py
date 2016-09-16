51328b3a1ab98ace36563b0487bb4d91a96dd0a5
Matthew Miller <mattdeanmiller@gmail.com>
2016-09-15 20:01:43 -0700

    Sends back test from direct messages, start of wolfram api

--- listen.py
+++ listen.py
@@ -1,12 +1,14 @@
 import twitter
 import time
 import sys
+import tungsten
 from OAuthSettings import settings
 
 consumer_key = settings['consumer_key']
 consumer_secret = settings['consumer_secret']
 access_token_key = settings['access_token_key']
 access_token_secret = settings['access_token_secret']
+wolfram_key = settings['wolfram_key']
 
 num_arg = len(sys.argv)
 if (num_arg > 1) :
@@ -26,6 +28,17 @@ id = f.read()
 f.close()
 f = open( 'last_msg_id.txt', 'w')
 
+client = tungsten.Tungsten(wolfram_key)
+print client
+result = client.query('pi')
+print result
+print len(result.pods)
+print result.error
+
+for pod in result.pods:
+	print 'here'
+	print pod.title
+
 count = 0
 while (count < num) :
 	try:
@@ -36,10 +49,21 @@ while (count < num) :
 		access_token_secret = access_token_secret)
 	
 		messages = api.GetDirectMessages(since_id=id)
+
+		update = True
 		for msg in messages:
    			print msg.text
 			print msg.id
-			id = msg.id
+			if (update) :
+				id = msg.id
+				update = False;
+			api.PostDirectMessage(msg.text, screen_name=msg.sender_screen_name)
+			
+			result = client.query(msg.text)
+			for pod in result.pods:
+        			print 'here'
+        			print pod.title			
+				api.PostDirectMessage(pd.title, screen_name='milmd90')
 
 	except twitter.TwitterError:
 		print 'error listening!'
